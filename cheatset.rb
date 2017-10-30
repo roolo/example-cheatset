131a854756d1a109f26750a5cf982cd5df25fc66
Mailo Světel <development@rooland.cz>
2017-10-30 22:09:06 +0100

    Moar code entries

--- cheatset.rb
+++ cheatset.rb
@@ -26,6 +26,47 @@ cheatsheet do
 
   category do
     id 'Code'
+
+    entry do
+      name 'Code sample'
+      notes <<-'END'
+        ```ruby
+        sample = "You can include code snippets as well"
+        ```
+        Or anything else **Markdown** or HTML.
+      END
+    end
+
+    entry do
+      name 'Code sample'
+      notes <<-'END'
+        ```ruby
+        sample = "You can include code snippets as well"
+        ```
+        Or anything else **Markdown** or HTML.
+      END
+    end
+
+    entry do
+      name 'Code sample'
+      notes <<-'END'
+    ```ruby
+    sample = "You can include code snippets as well"
+    ```
+    Or anything else **Markdown** or HTML.
+      END
+    end
+
+    entry do
+      name 'Code sample'
+      notes <<-'END'
+        ```ruby
+        sample = "You can include code snippets as well"
+        ```
+        Or anything else **Markdown** or HTML.
+      END
+    end
+
     entry do
       name 'Code sample'
       notes <<-'END'
